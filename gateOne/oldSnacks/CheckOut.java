98e1c4fe8cdd022eb3e520de0f1d516e3960186b
Iceflame07 <Chibizz007@gmail.com>
2024-11-04 13:04:53 +0100

    <checkOut>

---
+++ gateOne/oldSnacks/CheckOut.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+  public class checkOut {
+   public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the customer name:");
+    String customer = input.nextLine();
+
+    System.out.print("Enter the items bought:");
+    String items = input.nextLine();
+
+    System.out.print("How many pieces:");
+    int pieces = input.nextInt();
+
+    System.out.print("Enter the unitPrice:");
+    int price = input.nextLine();
+
+    System.out.print("Add more items:");
+    String items = input.nextLine();
+
+    String Yes = "1";
+    String No = "2";
+
+    for(int i = 0; i < yes; i++)
+     for(int j = 0; j < No; j++)
+
+    if (i == 1){
+	return "1";
+  } else if{
+	return "2";
+  } else {
+	return "Invalid";
+  }
+
+    System.out.print("Enter cashier name:");
+    String cashier = input.nextLine();
+
+    int discount = 0;
+
+    System.out.print("How much discount will you get:");
+    int discount = input.nextInt();
+
+    System.out.println("The amount paid:");
+    int amount = input.nextInt();
+
+    int Bill Total = items - unitPrice; 
+    int Balance = amount - discount;
+
+	return "balance";
+
+    System.ou.print("THANK YOU FOR YOUR PATRONAGE!!!");
+    String patronage = input.nextLine();
+
+   }
+
+}
